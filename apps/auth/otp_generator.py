2ba2b857c351e2e3ff68068ddd5f3ba44b6a20d6
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-10-05 19:18:10 +0330

    feat: add OTP service

---
+++ apps/auth/otp_generator.py
@@ -0,0 +1,18 @@
+from random import randint
+from apps.auth.email_service import EmailService
+
+
+class OTPHandler:
+    otp_expiry_minutes = 2
+
+    def __init__(self, email_service: EmailService):
+        self.email_service = email_service
+
+    def generate_otp(self):
+        otp = randint(1000, 9999)
+        return otp
+
+    def send(self, email: str):
+        otp = self.generate_otp()
+        self.email_service.send_otp_email(email, otp)
+        return otp
