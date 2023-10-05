85ec3f84e5e2696b833a497d50b5cdec52ae385b
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-10-05 19:18:02 +0330

    feat: add email service

---
+++ apps/auth/email_service.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.message import EmailMessage
+
+class EmailService:
+    def __init__(self, smtp_server, smtp_port, sender_email, sender_password):
+        self.smtp_server = smtp_server
+        self.smtp_port = smtp_port
+        self.sender_email = sender_email
+        self.sender_password = sender_password
+
+    def send_otp_email(self, recipient_email, otp):
+        msg = EmailMessage()
+        msg.set_content(f'Your OTP is: {otp}')
+        msg['Subject'] = 'OTP for Registration'
+        msg['From'] = self.sender_email
+        msg['To'] = recipient_email
+
+        server = smtplib.SMTP(self.smtp_server, self.smtp_port)
+        server.starttls()
+        server.login(self.sender_email, self.sender_password)
+
+        server.send_message(msg)
+        server.quit()
--- apps/core/settings.py
+++ apps/core/settings.py
@@ -12,6 +12,11 @@ ALGORITHM = "HS256"
 ACCSES_TOKEN_LIFETIME = 3600  # seconds
 REFRESH_TOKEN_LIFETIME = 86400 * 365  # seconds
 
+# Email Config
+SMTP_SERVER = config('SMTP_SERVER')
+SMTP_PORT = config('SMTP_PORT')
+SENDER_EMAIL = config('SENDER_EMAIL')
+EMAIL_PASSWORD = config('EMAIL_PASSWORD')
 
 class Settings(BaseModel):
     authjwt_secret_key: str = "secret"
