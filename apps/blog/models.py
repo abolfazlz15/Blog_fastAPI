d7209a50cd07901bbd25a430b2383881d4c95b18
Afshar Sharifi <afsharsharifi2020@gmail.com>
2023-10-07 12:50:39 +0330

    feat: adding schema_extra for blog

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from typing import List
 
 from beanie import Document, Link, PydanticObjectId
-from typing import List
 from bson import ObjectId
+from fastapi import FastAPI, File, UploadFile
+from pydantic import Field
 
 
 class CategorySchema(Document):
@@ -22,10 +24,10 @@ class BlogSchema(Document):
     Schema for Blog
     """
 
-    title: str
-    slug: str
+    title: str = Field(max_length=150)
+    slug: str = Field(max_length=150)
     author: PydanticObjectId
-    cover_image: str | None
+    cover_image: UploadFile = File()
     content: str
     category: Link[CategorySchema]
     viewers: list[str]
@@ -39,4 +41,14 @@ class BlogSchema(Document):
         name = "blogs"
 
     class Config:
-        orm_mode = True
+        schema_extra = {
+            "example": {
+                "title": "How to Create Blog usng FastAPI?",
+                "slug": "how-to-create-blog-usng-fastapi",
+                "cover_image": "Image File",
+                "content": "<h1>Hello</h1>",
+                "category": "ObjectId",
+                "related_blogs": ["", ""],
+                "publish_at": datetime.now(),
+            }
+        }
