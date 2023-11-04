230ee724b3bc9d8e2e3f23b88d3e3ac411f30540
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-04 09:47:22 +0330

    refactor: change login endpoint function for new DB

--- apps/auth/routers.py
+++ apps/auth/routers.py
@@ -1,9 +1,9 @@
 import time
 from datetime import timedelta
 from typing import Annotated
+from sqlalchemy.orm import Session
 
 import psycopg2
-from apps.auth.models import TokenSchema, UserSchema
 from apps.auth.security import (
     authenticate_user,
     create_access_token,
@@ -12,23 +12,21 @@ from apps.auth.security import (
 from apps.core.settings import ACCSES_TOKEN_LIFETIME
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from apps.core.database import get_db
+from apps.auth import schemas
 
-from apps.auth.models import TokenSchema, UserSchema, UserCreateRequestSchema
-from apps.auth.security import (authenticate_user, create_access_token,
-                                get_current_user)
-from apps.core.db import get_user_collection
-from apps.core.settings import ACCSES_TOKEN_LIFETIME
 
 router = APIRouter()
 
 
 
-@router.post('/login/', response_model=TokenSchema)
-async def Login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: UserSchema = Depends(get_user_collection)):
-    user = authenticate_user(form_data.username, form_data.password, db)
+@router.post('/login/', response_model=schemas.Token)
+async def Login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Incorrect username or password')
     
     access_token_expires = timedelta(seconds=ACCSES_TOKEN_LIFETIME)
-    token = create_access_token(user.username, user.email, access_token_expires)
+    access_token = create_access_token(user.username, user.email, access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
