290601453dee3abb4034507cf1924a25506e4cd3
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-04 09:41:09 +0330

    fix: add created_at field for user model

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -1,15 +1,18 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import TIMESTAMP, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import Boolean
+from sqlalchemy.sql.expression import text
 
 from apps.core.database import Base
 
+
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     id: int = Column(Integer, primary_key=True)
     username: str = Column(String(50), unique=True)
     email: str = Column(String, nullable=False, unique=True)
     hashed_password: str = Column(String, nullable=False)
     is_active: bool = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from apps.blog import routers as BlogRoutes
 from apps.core import database, models
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
-
+from apps.auth import routers as AuthRoutes
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
 
-@app.get("/", tags=["Root"])
+@app.get('/', tags=['Root'])
 def read_root():
-    return {"message": "This is Root"}
+    return {'message': 'This is Root'}
 
 
-@app.get("/sqlalchemy")
+@app.get('/sqlalchemy')
 def test_root(db: Session = Depends(database.get_db)):
-    return {"status": "OK"}
+    return {'status': 'OK'}
 
 
-app.include_router(BlogRoutes.router, tags=["Blog"])
+app.include_router(BlogRoutes.router, tags=['Blog'])
+app.include_router(AuthRoutes.router, tags=['auth'])
