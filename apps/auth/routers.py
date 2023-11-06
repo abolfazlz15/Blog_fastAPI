e4d89ed49a34344b3899c7779e6dc6dd897e0ff3
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-06 22:37:33 +0330

    feat: add genrate_otp & email config

--- apps/auth/routers.py
+++ apps/auth/routers.py
@@ -1,23 +1,35 @@
-import time
 from datetime import timedelta
 from typing import Annotated
 from sqlalchemy.orm import Session
+from apps.auth.email_service import EmailService
+from apps.auth.otp_generator import OTPHandler
+import json
 
-import psycopg2
 from apps.auth.security import (
     authenticate_user,
     create_access_token,
-    get_current_user,
+    get_user_by_email,
+    get_user_by_username,
 )
 from apps.core.settings import ACCSES_TOKEN_LIFETIME
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from apps.core.database import get_db
+from apps.core.database import get_db, redis_client
 from apps.auth import schemas
+from apps.core import settings
+
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
 
 
-router = APIRouter()
+email_service = EmailService(smtp_server=settings.SMTP_SERVER,
+                             smtp_port=int(settings.SMTP_PORT),
+                             sender_email=settings.SENDER_EMAIL,
+                             sender_password=settings.EMAIL_PASSWORD)
 
+otp_handler = OTPHandler(email_service)
 
 
 @router.post('/login/', response_model=schemas.Token)
@@ -26,7 +38,25 @@ async def Login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Incorrect username or password')
-    
+
     access_token_expires = timedelta(seconds=ACCSES_TOKEN_LIFETIME)
-    access_token = create_access_token(user.username, user.email, access_token_expires)
+    access_token = create_access_token(
+        user.username, user.email, access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post('/regiser/')
+async def genrate_otp(user_data: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_username = get_user_by_username(db, username=user_data.username)
+    db_email = get_user_by_email(db, email=user_data.email)
+    if db_username:
+        raise HTTPException(
+            status_code=400, detail="Username already registered"
+        )
+    elif db_email:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    otp = otp_handler.send(user_data.email)
+    
+    user_data_json = json.dumps(user_data.dict())
+    redis_client.setex(otp, 120, user_data_json)
+    return {'message': 'OTP code sent'}
--- apps/auth/schemas.py
+++ apps/auth/schemas.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel, validator
 class UserBase(BaseModel):
     username: str
     email: str
-    is_active: bool = False
 
 
 class UserCreate(UserBase):
