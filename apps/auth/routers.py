bd536e9b0395667bd1bf741dee6b78e90d92fc2e
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-07 17:27:09 +0330

    feat: add verify otp endpont

--- apps/auth/routers.py
+++ apps/auth/routers.py
@@ -8,6 +8,7 @@ import json
 from apps.auth.security import (
     authenticate_user,
     create_access_token,
+    create_user,
     get_user_by_email,
     get_user_by_username,
 )
@@ -36,8 +37,7 @@ otp_handler = OTPHandler(email_service)
 async def Login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Incorrect username or password')
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, 'Incorrect username or password')
 
     access_token_expires = timedelta(seconds=ACCSES_TOKEN_LIFETIME)
     access_token = create_access_token(
@@ -45,18 +45,36 @@ async def Login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.post('/regiser/')
+@router.post('/regiser/', status_code=status.HTTP_200_OK)
 async def genrate_otp(user_data: schemas.UserCreate, db: Session = Depends(get_db)):
     db_username = get_user_by_username(db, username=user_data.username)
     db_email = get_user_by_email(db, email=user_data.email)
     if db_username:
-        raise HTTPException(
-            status_code=400, detail="Username already registered"
-        )
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail='Username already registered')
     elif db_email:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, 'Email already registered')
+   
     otp = otp_handler.send(user_data.email)
-    
-    user_data_json = json.dumps(user_data.dict())
+    user_data_dict = user_data.dict()
+    user_data_dict['otp'] = otp
+    user_data_json = json.dumps(user_data_dict)
     redis_client.setex(otp, 120, user_data_json)
     return {'message': 'OTP code sent'}
+
+
+@router.post(
+        '/verify-otp/',
+        response_model=schemas.UserOut,
+        status_code=status.HTTP_201_CREATED,
+)
+async def verify_otp(otp_code_data: schemas.OTPCode, db: Session = Depends(get_db)):
+    otp_code = otp_code_data.otp_code
+    user_data_bytes = redis_client.get(str(otp_code))
+
+    if not user_data_bytes:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, 'Code invalid or expire')
+
+    user_data_json = user_data_bytes.decode('utf-8')
+    user_data_dict = json.loads(user_data_json)
+    user = create_user(db, user_data_dict)
+    return user
