9053cc97e66f41d060efeb6c524313dd81520138
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-12-15 18:50:51 +0330

    feat: add change user password schema

--- apps/auth/schemas.py
+++ apps/auth/schemas.py
@@ -50,3 +50,24 @@ class ResetPasswordIn(BaseModel):
         if new_password is not None and new_password_confirm is not None and new_password != new_password_confirm:
             raise ValueError('passwords do not match')
         return self
+
+
+class ChangePasswordIn(BaseModel):
+    current_password: str
+    new_password: str
+    new_password_confirm: str
+
+    @model_validator(mode='after')
+    def validate_password(self):
+        current_password = self.current_password
+        new_password = self.new_password
+        new_password_confirm = self.new_password_confirm
+        if new_password is not None and new_password_confirm is not None and new_password != new_password_confirm:
+            if current_password == new_password:
+                raise ValueError('your new password cant be like your current password')
+            raise ValueError('passwords do not match')
+        return self
+    
+
+class UserEmail(BaseModel):
+    email: str
