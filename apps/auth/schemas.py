7e4ee6351587eb4e856dcb6151032e5a4cfc3c6d
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-12-18 10:45:06 +0330

    feat: add schema and custom type for password

--- apps/auth/schemas.py
+++ apps/auth/schemas.py
@@ -1,5 +1,17 @@
 from typing import Any
-from pydantic import BaseModel, field_validator, EmailStr, ConfigDict, model_validator
+from pydantic import BaseModel, BeforeValidator, field_validator, EmailStr, ConfigDict, model_validator, validator
+from fastapi import HTTPException
+from typing import Annotated
+
+
+def validate_password_length(v):
+    min_length = 8  # Change this to your desired minimum length
+    if len(v) < min_length:
+        raise HTTPException(status_code=400, detail=f"Password length should not be smaller than {min_length} characters")
+    return v
+
+PasswordStr = Annotated[str, BeforeValidator(validate_password_length)]
+
 
 
 class UserBase(BaseModel):
@@ -54,20 +66,21 @@ class ResetPasswordIn(BaseModel):
 
 class ChangePasswordIn(BaseModel):
     current_password: str
-    new_password: str
-    new_password_confirm: str
+    new_password: PasswordStr
+    new_password_confirm: PasswordStr
 
     @model_validator(mode='after')
-    def validate_password(self):
+    def validate_passwords(self):
         current_password = self.current_password
         new_password = self.new_password
         new_password_confirm = self.new_password_confirm
+        
         if new_password is not None and new_password_confirm is not None and new_password != new_password_confirm:
             if current_password == new_password:
                 raise ValueError('your new password cant be like your current password')
             raise ValueError('passwords do not match')
-        return self
-    
+        return self        
+
 
 class UserEmail(BaseModel):
     email: str
